9614f842537b41b70f536f12057682ca36c45b3e
arun <arunjyoti.sarmah@gmail.com>
2024-09-10 01:16:02 -0400

    fix: resolve build tagging

--- src/components/layouts/MainHeader.tsx
+++ src/components/layouts/MainHeader.tsx
@@ -16,7 +16,7 @@ import Logo from "../Logo";
 import SearchBox from "../SearchBox";
 import NetflixNavigationLink from "../NetflixNavigationLink";
 
-const pages = ["Arun's Fav Movie List", "Movies", "Tv Shows"];
+const pages = ["Arun's Favorite Movie List", "Movies", "Tv Shows"];
 
 const MainHeader = () => {
   const isOffset = useOffSetTop(APP_BAR_HEIGHT);
