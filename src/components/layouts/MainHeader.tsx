fb9758356d5d52af34fef7b833822becc6f77785
arun <arunjyoti.sarmah@gmail.com>
2024-09-10 13:26:40 -0400

    feat!: test

--- src/components/layouts/MainHeader.tsx
+++ src/components/layouts/MainHeader.tsx
@@ -16,7 +16,7 @@ import Logo from "../Logo";
 import SearchBox from "../SearchBox";
 import NetflixNavigationLink from "../NetflixNavigationLink";
 
-const pages = ["Arun's List", "Great 7.1.0 Movies", "Latest Tv Shows"];
+const pages = ["Arun's List", "Great 8.0.0 Movies", "Latest Tv Shows"];
 
 const MainHeader = () => {
   const isOffset = useOffSetTop(APP_BAR_HEIGHT);
